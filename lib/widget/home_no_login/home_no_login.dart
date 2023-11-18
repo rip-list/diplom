03752c323fe4c91a0d6cef62875b43f0c5abf74e
John Doe <valeraarealv79@gamil.com>
2023-11-18 07:05:53 +0600

    padding

--- lib/widget/home_no_login/home_no_login.dart
+++ lib/widget/home_no_login/home_no_login.dart
@@ -22,24 +22,17 @@ class HomeNoLogin extends StatelessWidget {
         width: MediaQuery.of(context).size.width,
         height: MediaQuery.of(context).size.height,
         color: AppColors.secondary,
-        child: const SingleChildScrollView(
-          scrollDirection: Axis.horizontal,
-          child: Padding(
-            padding: EdgeInsets.all(20.0),
+        child: const Padding(
+          padding: EdgeInsets.symmetric(horizontal: 20.0, vertical: 20.0),
+          child: SingleChildScrollView(
+            scrollDirection: Axis.horizontal,
             child: Row(
               children: [
-                CusBox(
-                  height: 0.9,
-                  width: 0.4,
-                ),
-                SizedBox(width: 20, height: 20),
-                Column(
-                  children: [
-                    CusBox(height: 0.439, width: 0.57),
-                    SizedBox(width: 20, height: 20),
-                    CusBox(height: 0.44, width: 0.57),
-                  ],
-                ),
+                CusBox(height: 0.9, width: 0.4),
+                SizedBox(width: 20),
+                CusBox(height: 0.439, width: 0.56),
+
+                // CusBox(height: 0.44, width: 0.57),
               ],
             ),
           ),
